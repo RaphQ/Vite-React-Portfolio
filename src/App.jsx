811a7b60bb36a4fd71660624c454dbb9db1c8106
Raphael <raphaeljohnquimson@gmail.com>
2023-06-07 23:54:39 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Sidenav from './components/Sidenav';
+import Main from './components/Main';
+import Work from './components/Work';
+
+function App() {
+  return (
+    <div>
+      <Sidenav />
+      <Main />
+      <Work />
+    </div>
+  );
+}
+
+export default App;
